db1feb78acbdfa0c6ed141fe35dc2a907d1c444e
bettonhezron <hezronbet@gmail.com>
2025-01-03 17:37:57 +0300

    added saving method

--- src/main/java/org/betthezron/MultiplicationTable.java
+++ src/main/java/org/betthezron/MultiplicationTable.java
@@ -1,5 +1,7 @@
 package org.betthezron;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -51,61 +53,84 @@ public class MultiplicationTable {
         }
         scanner.close();
 
-        private static void generateSingleTable(Scanner scanner) {
-            try {
-                System.out.println("Enter the number:");
-               int number = scanner.nextInt();
 
-                System.out.println("Enter the range (e.g., 10 for 1 to 10): ");
-                int range = scanner.nextInt();
-
-                System.out.println("\nMultiplication table for " + number);
-
-                for (int i = 1; i <= range; i++) {
-                    System.out.println(number + "x" + i + "=" + (number * i));
-                }
-            }
-        }
-
-        if (choice == 1) {
-            int number = 0;
-            try {
-
-
-
-            } catch (InputMismatchException e) {
-                System.out.println("Invalid Input.Please enter a number.");
             }
 
+    private static void generateSingleTable(Scanner scanner) {
+        try {
+            System.out.println("Enter the number:");
+            int number = scanner.nextInt();
 
             System.out.println("Enter the range (e.g., 10 for 1 to 10): ");
             int range = scanner.nextInt();
 
+            System.out.println("\nMultiplication table for " + number);
 
+            for (int i = 1; i <= range; i++) {
+                System.out.printf("%2d x %2d = %3dn",number,i,number*i);
+            }
+        } catch (InputMismatchException e){
+            System.out.println("Invalid Input.Please enter valid numbers.");
+            scanner.next();
 
-        } else if (choice == 2) {
+        }
+
+    }
+    private static void generateRangeOfTables(Scanner scanner) {
+        try {
             System.out.println("Enter the starting number: ");
             int start = scanner.nextInt();
 
-            System.out.println("Enter the ending number: ");
+            System.out.println("Enter the ending number");
             int end = scanner.nextInt();
 
-            System.out.println("Enter the range (e.g., 10 for 1 to 10): ");
+            System.out.println("Enter the range(e.g., 10 for 1 to 10):");
             int range = scanner.nextInt();
 
-            for (int num = start; num < end; num++ ){
-                System.out.println("\nMultiplication Table for " + num);
+            for (int num = start; num <= end; num++){
+                System.out.println("\nMultiplication Table for" + num);
 
-                for (int i = 1; i<= range; i++){
-                    System.out.println(num + "x" + i + "=" + (num * i));
+                for (int i= 1; i<= range; i++){
+                    System.out.printf("%2d x %2d = %3dn", num, i,num*i);
                 }
             }
 
-        } else {
-            System.out.println("Invalid Choice.");
         }
-        scanner.close();
+        catch
+         (InputMismatchException e){
+            System.out.println("Invalid input. Please enter valid numbers");
+            scanner.next();
+
+        }
+
+    }
+
+    private static void saveTablesToFile(Scanner scanner) {
+        try {
+            System.out.println("Enter starting number:");
+            int start = scanner.nextInt();
+
+            System.out.println("Enter the ending number");
+            int end = scanner.nextInt();
+
+            System.out.println("Enter the range(e.g., 10 for 1 to 10):");
+            int range = scanner.nextInt();
+
+            String filename = scanner.next();
+            FileWriter writer = new FileWriter(filename);
+
+        }catch (IOException e) {
+            System.out.println("Error saving to file:" + e.getMessage());
+        } catch (InputMismatchException e){
+            System.out.println("Invalid input. Please enter valid numbers.");
+            scanner.next();
+        }
+
     }
 
 
+
 }
+
+
+
